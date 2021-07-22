a7a898bdbc4c02995cfc4a6921b3133ee1e04987
Tomáš Korenko <radixxko@gmail.com>
2021-07-22 11:57:37 +0200

    Version 1.0.1

--- test/test.js
+++ test/test.js
@@ -3,8 +3,8 @@
 describe( 'Tests', ( done ) =>
 {
     //require( './tests/client' );
-    //require( './tests/server' );
-    require( './tests/client_server' );
+    require( './tests/server' );
+    //require( './tests/client_server' );
     //require( './tests/client_pong' );
     //require( './tests/server_ping' );
 });
